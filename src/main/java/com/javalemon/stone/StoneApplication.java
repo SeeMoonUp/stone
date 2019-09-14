3f62b5cb475ac00c5506a43c7410fec058b497fd
limeng <seemoonup@gmail.com>
2019-09-14 19:13:18 +0800

    后台系统初始化

--- src/main/java/com/javalemon/stone/StoneApplication.java
+++ src/main/java/com/javalemon/stone/StoneApplication.java
@@ -1,9 +1,11 @@
 package com.javalemon.stone;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.javalemon.stone.dao.mapper") //扫描的mapper
 public class StoneApplication {
 
 	public static void main(String[] args) {
