36b9acfe3104c2de5a62408b21ebdced8ae9bba6
limeng <seemoonup@gmail.com>
2019-10-27 23:02:59 +0800

    文章上传

--- src/main/java/com/javalemon/stone/model/dto/ArticleVideoDTO.java
+++ src/main/java/com/javalemon/stone/model/dto/ArticleVideoDTO.java
@@ -1,8 +1,10 @@
 package com.javalemon.stone.model.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -14,6 +16,8 @@ import java.util.Date;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleVideoDTO {
 
     private int id;
